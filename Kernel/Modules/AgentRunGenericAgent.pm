c3dc675e22999e6e0b0f0e6444e6e00e9ec9a341
reneeb <info@perl-services.de>
2018-01-30 16:07:08 +0100

    initial commit

---
+++ Kernel/Modules/AgentRunGenericAgent.pm
@@ -0,0 +1,66 @@
+# --
+# Copyright (C) 2018 Perl-Services.de, http://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AgentRunGenericAgent;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = qw(
+    Kernel::Config
+    Kernel::Output::HTML::Layout
+    Kernel::System::Web::Request
+    Kernel::System::GenericAgent
+);
+
+use Kernel::Language qw(Translatable);
+use Kernel::System::VariableCheck qw(:all);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $LayoutObject       = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $ParamObject        = $Kernel::OM->Get('Kernel::System::Web::Request');
+    my $ConfigObject       = $Kernel::OM->Get('Kernel::Config');
+    my $GenericAgentObject = $Kernel::OM->Get('Kernel::System::GenericAgent');
+
+    my @Runnables    = @{ $ConfigObject->Get('TicketMenuRunGenericAgent::Runnable')  || [] };
+    my $GenericAgent = $ParamObject->GetParam( Param => 'GA' );
+
+    # check needed stuff
+    if ( !grep{ $_ eq $GenericAgent }@Runnables ) {
+
+        # error page
+        return $LayoutObject->ErrorScreen(
+            Message => Translatable('No valid GenericAgent given!'),
+            Comment => Translatable('Please contact the admin.'),
+        );
+    }
+
+    my $Run = $GenericAgentObject->JobRun(
+        Job          => $GenericAgent,
+        OnlyTicketID => $Self->{TicketID},
+        UserID       => $Self->{UserID},
+    );
+    
+    return $LayoutObject->Redirect(
+        OP => 'Action=AgentTicketZoom&TicketID=' . $Self->{TicketID},
+    );
+}
+
+1;
